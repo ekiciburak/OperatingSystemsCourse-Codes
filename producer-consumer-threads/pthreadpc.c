3eaa307d95be3972acc0e36cae316c1efc98f93c
ekiciburak <ekcburak@hotmail.com>
2023-05-23 14:42:45 +0300

    n producers + m consumers...

--- producer-consumer-threads/pthreadpc.c
+++ producer-consumer-threads/pthreadpc.c
@@ -7,8 +7,9 @@
 #include "semaphore.h"     // sem_t
 #include "pthread.h"
 
-#define size 5
-#define psize 4096
+#define size 15
+#define NUMOFPROD 5
+#define NUMOFCONS 5
 
 struct circular
 {
@@ -18,106 +19,114 @@ struct circular
     int counter;
     sem_t empty;
     sem_t full;
+    sem_t lock;
 };
 
 typedef struct circular CA;
 
-void initCA(CA *);
-void addCA(CA *);
-void dropCA(CA *);
-void printCA(char, CA *);
+CA a;
+
+void initCA(void);
+void addCA(int *);
+void dropCA(int *);
+void printCA(char, int);
 
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
 
-    CA *a = malloc(sizeof(CA));
-    initCA(a);
+    pthread_t prod[NUMOFPROD], cons[NUMOFCONS];
+    int idp[NUMOFPROD];
+    int idc[NUMOFCONS];
 
-    pthread_t prod, cons;
+    initCA();
 
-    pthread_create(&prod, NULL, (void *) addCA, a);
-    pthread_create(&prod, NULL, (void *) dropCA, a);
+    for (size_t i = 0; i < NUMOFPROD; i++)
+    {
+        idp[i] = i;
+        pthread_create(&prod[i], NULL, (void *) addCA, &idp[i]);
+    }
 
-    pthread_join(prod, NULL);
-    pthread_join(cons, NULL);
+    for (size_t i = 0; i < NUMOFCONS; i++)
+    {
+        idc[i] = i;
+        pthread_create(&cons[i], NULL, (void *) dropCA, &idc[i]);
+    }
+
+
+    for (size_t i = 0; i < NUMOFPROD; i++)
+        pthread_join(prod[i], NULL);
+    
+    for (size_t i = 0; i < NUMOFCONS; i++)
+        pthread_join(cons[i], NULL);
+
 
     return 0;
 }
 
-void initCA(CA *a)
+void initCA()
 {
-    (*(a)).in = 0;
-    (*(a)).out = 0;
+    a.in = 0;
+    a.out = 0;
     for (size_t i = 0; i < size; i++)
-        (*(a)).buffer[i] = 0;
-    (*(a)).counter = 0;
-    sem_init(&(*(a)).empty, 1, 5);
-    sem_init(&(*(a)).full, 1, 0);
+        a.buffer[i] = 0;
+    a.counter = 0;
+    sem_init(&a.empty, 1, size);
+    sem_init(&a.full, 1, 0);
+    sem_init(&a.lock, 1, 1);
 }
 
-void addCA(CA *a)
+void addCA(int *identity)
 {
+    int id = *identity;
     int next;
 
     while (true)
     {
-        sleep(5);
-        //while(((*(a)).in + 1) % size == (*(a)).out); 
-        //while((*(a)).counter == size);
-        /*
-            sem_wait(S)
-            {
-                while(S <= 0);
-                S--;
-            }
-        */
-        sem_wait(&(*(a)).empty);
+        sleep(1);
+        sem_wait(&a.empty);
+        sem_wait(&a.lock);
         next = (rand() % 9) + 1;
-        (*(a)).buffer[(*(a)).in] = next;
-        (*(a)).in = ((*(a)).in + 1) % size;
-        (*(a)).counter++;
-        printCA('p', a);
-        sem_post(&(*(a)).full);
-        /*
-            sem_post(S)
-            {
-                S++;
-            }
-        */
+        a.buffer[a.in] = next;
+        a.in = (a.in + 1) % size;
+        a.counter++;
+        printCA('p', id);
+        sem_post(&a.lock);
+        sem_post(&a.full);
     }
     
 }
 
-void dropCA(CA *a)
+void dropCA(int *identity)
 {
+    int id = *identity;
     int prev;
 
     while (true)
     {
-        sleep(1);
-        //while((*(a)).in == (*(a)).out);
-        //while((*(a)).counter == 0);
-        sem_wait(&(*(a)).full);
-        prev = (*(a)).buffer[(*(a)).out];
-        (*(a)).buffer[(*(a)).out] = 0;
-        (*(a)).out = ((*(a)).out + 1) % size;
-        (*(a)).counter--;
-        printCA('c', a);
-        sem_post(&(*(a)).empty);
+        sleep(10);
+        sem_wait(&a.full);
+        sem_wait(&a.lock);
+        prev = a.buffer[a.out];
+        a.buffer[a.out] = 0;
+        a.out = (a.out + 1) % size;
+        a.counter--;
+        printCA('c', id);
+        sem_post(&a.lock);
+        sem_post(&a.empty);
     }
 
 }
 
-void printCA(char c, CA *a)
+void printCA(char c, int k)
 {
-    printf("%c -- {", c);
+    printf("%c%d -- {", c, k);
     for (size_t i = 0; i < size; i++)
     {
         if(i < size - 1)
-            printf("%d,", (*(a)).buffer[i]);
+            printf("%d,", a.buffer[i]);
         else
-            printf("%d", (*(a)).buffer[i]);
+            printf("%d", a.buffer[i]);
     }
-    printf("} -- (%d,%d) -- %d\n", (*(a)).in, (*(a)).out, (*(a)).counter);
+    printf("} -- (%d,%d) -- %d\n", a.in, a.out, a.counter);
 }
